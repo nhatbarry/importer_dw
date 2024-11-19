5a52acc90f3df1501dfab281a2e6ab3b9f709fbb
nhatbarry <nhatbarry@gmail.com>
2024-11-19 20:59:33 +0700

    03

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,5 +2,6 @@ SELECT
   order_line_id as sales_order_line_key
   , quantity
   , unit_price
+  , stock_item_id as product_key
   , quantity * unit_price as gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
