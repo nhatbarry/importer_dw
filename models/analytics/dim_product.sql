233e555a9c92269f0cf10ff2e10bb1d3574f874c
nhatbarry <nhatbarry@gmail.com>
2024-11-19 22:33:01 +0700

    5

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,32 @@
+with dim_product__source
+as
+(
+  select * from `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+, dim_product__rename_col
+as
+(
+  select
+    stock_item_id as product_key
+    , stock_item_name as product_name
+    , brand as brand_name
+  from dim_product_source
+)
+
+, dim_product__cast
+as
+(
+  select
+  cast(product_key as integer) as product_key
+  , cast(product_name as string) as product_name
+  , cast(brand_name as string) as brand_name
+  from dim_product_rename_col
+)
+
+
 SELECT 
-  stock_item_id as product_key
-  , stock_item_name as product_name
-  , brand as brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  product_key
+  , product_name
+  , brand_name
+FROM dim_product_cast
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,47 @@
-SELECT 
-  order_line_id as sales_order_line_key
+with fact_sales_order_line__src
+as
+(
+  select * from `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, fact_sales_order_line__rename
+as
+(
+  SELECT 
+    order_line_id as sales_order_line_key
+  , quantity as quantity
+  , unit_price as unit_price
+  , stock_item_id as product_key
+FROM fact_sales_order_line__src
+)
+
+, fact_sales_order_line__cast_type
+as
+(
+  SELECT 
+  cast(sales_order_line_key as integer) as sales_order_line_key
+  , cast(quantity as integer) as quantity
+  , cast(unit_price as numeric) as unit_price
+  , cast(product_key as integer) as product_key
+FROM fact_sales_order_line__rename
+)
+
+, fact_sales_order_line__calculated_measure
+as
+(
+  select 
+  sales_order_line_key
   , quantity
   , unit_price
-  , stock_item_id as product_key
-  , quantity * unit_price as gross_amount
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+  , product_key
+  , (quantity * unit_price) as gross_amount
+  from fact_sales_order_line__cast_type
+)
+
+SELECT 
+  sales_order_line_key
+  , quantity
+  , unit_price
+  , product_key
+  , gross_amount
+FROM fact_sales_order_line__calculated_measure
